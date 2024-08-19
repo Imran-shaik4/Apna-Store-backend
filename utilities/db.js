9e013e9c3d84d35e140ddd1dbd3e1dbd9ad88d4b
imran <imran.smartcoder@gmail.com>
2024-08-19 20:33:47 +0530

    production ready

--- utilities/db.js
+++ utilities/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://imranshaik8333:8688929795Imran@cluster0.r405n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017", {
+        await mongoose.connect("mongodb+srv://imranshaik8333:8688929795Imran@cluster0.r405n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
